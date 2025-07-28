ae8b3057ad28d4e362dae93c32472c819b0e8dba
ISHWARYA SREE V <ishwaryasree.work@gmail.com>
2025-07-28 19:20:34 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = hemalatha301297@gmail.com
+EMAIL_HOST_PASSWORD = zkve tarv eijs illb
 
 DEFAULT_FROM_EMAIL = f'JobPortal <{EMAIL_HOST_USER}>'
